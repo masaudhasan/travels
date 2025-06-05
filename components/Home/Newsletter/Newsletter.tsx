60a6bb433429f48168d120b585f9b19441ded229
Md Masaud Hasan <mdmasaud58@gmail.com>
2025-06-05 15:53:43 +0600

    fixed deployment problem

--- components/Home/Newsletter/Newsletter.tsx
+++ components/Home/Newsletter/Newsletter.tsx
@@ -6,7 +6,7 @@ const Newsletter = () => {
     <div className='bg-black bt-16 pb-16 flex items-center justify-center w-full flex-col'>
         <BsEnvelopePaper className='w-16 h-16 mt-20 text-white'/>
         <h1 className='text-lg sm:text-xl md:text-2xl text-white font-semibold mt-10 tracking-widest'>Your Travel Journey Starts Here</h1>
-        <p className='mt-3 text-white text-xs sm:text-sm'>Sign up and we'll send the best deals to you</p>
+        <p className='mt-3 text-white text-xs sm:text-sm'>Sign up and we will send the best deals to you</p>
 
         {/* Input and Button */}
         <div className='w-full'>
